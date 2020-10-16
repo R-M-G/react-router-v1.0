51eced264be6acaab4c93bdda2a273e74d13273a
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-16 00:25:56 -0500

    imported Google fonts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom";
 
-
+import timeMachine from './time_machine.png';
 
 
 export class Home extends Component {
@@ -10,7 +10,7 @@ export class Home extends Component {
             <div>
                 <h1>MAKE IT HAPPEN</h1>
                 <h2>THE CHOICE IS YOURS</h2>
-                <br></br> 
+                <img className='time-machine' src={timeMachine} alt="Logo" /> 
                 <h2>MASTER OF THE PAST</h2>
                 <h3>Created from A Book By Steven Otfinoski</h3>
                 <Link to="/begin">
