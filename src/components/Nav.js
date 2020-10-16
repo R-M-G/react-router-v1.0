841fc55d65308286bcf4bd5fc4fcad32a405eb56
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-15 19:18:17 -0500

    styled links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,21 +5,16 @@ import {Link} from "react-router-dom";
 
 
 function Nav() {
+    const linkStyle = {
+        color: 'white'
+    }
   return (
     <nav>
       <h1>Logo</h1>
       <ul className="nav-links">
-        <Link to="/">
-            <li>Home</li>
-        </Link>
-
-        <Link to="/about">
-        <li>About</li>
-        </Link>
-
-        <Link to="/begin">
-        <li>Begin</li>
-        </Link>
+        <Link style={linkStyle} to="/"><li>Home</li></Link>
+        <Link style={linkStyle} to="/about"><li>About</li> </Link>
+        <Link style={linkStyle} to="/begin"><li>Begin</li></Link>
       </ul>
       </nav>
   );
